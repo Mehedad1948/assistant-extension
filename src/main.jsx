0171581c53ca03795d9ebee53100acd343e534e1
Mehrdad <mnourib13@gmail.com>
2025-10-01 19:09:41 +0330

    Fix: file type error

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
-import { AppProvider } from './context.js';
+import { AppProvider } from './context';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
